2853ee9781d7a6d10beb055ef53cbfe602d8a9cf
[dyad] <uzzalhossain.100@gmail.com>
2025-10-13 11:29:38 +0600

    [dyad] wrote 1 file(s)

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
@@ -8,7 +10,7 @@ const MainLayout: React.FC = () => {
 
   return (
     <div className="flex flex-col min-h-screen w-full">
-      <Header key={currentLanguage} /> {/* Add key that changes with language */}
+      <Header /> {/* Removed key={currentLanguage} */}
       <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
         <Outlet /> {/* This is where the child routes (Dashboard, News, AI, Contact) will render */}
       </main>
